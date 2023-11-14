ded1a4ce9a91d7c0a78318b62cb52d1c3593e86d
marco.bravo <marco.bravo@agrosat.cl>
2023-11-14 11:54:37 -0300

    Initializing my project 'portfolio frontend'

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+
+  return (
+    <>
+      <h1>My portfolio frontend! coming soon...</h1>
+      <p>first commit</p>
+    </>
+  );
+}
+
+export { App };
